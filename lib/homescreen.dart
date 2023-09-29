df33140c763639893a45e3e2934db49d1775be2d
Adomakojohn <johnkwakuadomako6@gmail.com>
2023-09-29 19:00:24 +0000

    few changes made

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
-import 'package:flip_card/flip_card.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:riddles_resorts/models.dart';
-import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,44 +12,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<Riddle> getRiddle() async {
-    final response =
-        await http.get(Uri.parse('https://api-ninjas.com/api/riddles'));
+  Future<Riddles?>?riddle ;
+  void clickGetButton(){
+  setState(() {
+    riddle=fetchRiddles();
+  });
+}
+  Future<Riddles> fetchRiddles() async {
+    final uri = Uri.parse("https://api.api-ninjas.com/v1/riddles");
+    final response = await http.get(uri);
+
     if (response.statusCode == 200) {
-      final Riddle = jsonDecode(response.body);
-      return Riddle.fromJson(Riddle);
+      return Riddles.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load the riddle');
+      throw Exception('failed to load riddle');
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: FutureBuilder<Riddle>(
-          future: getRiddle(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Text("Loading...");
-            }
-            if (snapshot.hasData) {
-              final riddle = snapshot.data;
-              return FlipCard(
-                direction: FlipDirection.HORIZONTAL,
-                front: Card(
-                  child: Text(
-                    '${riddle!.question}',
-                  ),
-                ),
-                back: Container(
-                  child: Text('${riddle.answer}'),
-                ),
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
+      body:  SafeArea(
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder(
+              future: fetchRiddles(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.connectionState == ConnectionState.none) {
+                  return Container();
+                } else {
+                  if (snapshot.hasData) {
+                    return buildDataWidget(context, snapshot);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  } else {
+                    return Container();
+                  }
+                }
+              },
+            ),
+             SizedBox(
+            width: 200,
+            child: ElevatedButton(onPressed: () => clickGetButton(), child:  const Text("get riddle"),
+           ))
+          ],
         ),
       ),
     );
--- lib/models.dart
+++ lib/models.dart
@@ -1,15 +1,53 @@
-class Riddle {
-  final String title;
-  final String question;
-  final String answer;
+import 'package:flutter/material.dart';
+import 'package:flip_card/flip_card.dart';
 
-  Riddle({required this.question, required this.answer , required this.title});
+class Riddles {
+    String question;
+    String answer;
 
-  factory Riddle.fromJson(Map<String, dynamic> json) {
-    return Riddle(
-      title: json['title'],
-      question: json['question'],
-      answer: json['answer'],
+    Riddles({
+        required this.question,
+        required this.answer,
+    });
+
+    factory Riddles.fromJson(Map<String ,dynamic> json) => Riddles(
+        question: json["question"],
+        answer: json["answer"],
     );
-  }
+
+   
 }
+
+Widget buildDataWidget(context , snapshot)=>  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+              padding: const EdgeInsets.all(10),
+              child: FlipCard(
+                direction: FlipDirection.HORIZONTAL,
+                flipOnTouch: true,
+                front: ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 600,
+                    width: 650,
+                    color: const Color(0xFF006666),
+                    child:Text(snapshot.data.question) ,
+                  ),
+                ),
+                back: ClipRRect(
+                  borderRadius: BorderRadius.circular(25),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 600,
+                    width: 650,
+                    color: const Color(0xFF006666),
+                    child: Text(snapshot.data.answer),
+                  ),
+                ),
+              )),
+        ],
+      );
+
+
